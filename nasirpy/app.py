4894ba89e6bb5607b062ff499631e40ada796948
NasirXpython <Nasir.Iqbal.Dev@gmail.com>
2022-06-17 14:30:00 +0400

    Add  form and json input handing

--- nasirpy/app.py
+++ nasirpy/app.py
@@ -2,11 +2,13 @@ from typing import Callable, Dict, List, Optional, Union, Tuple
 from .request import Request
 from .response import Response
 from .utils import match_route
+from .exceptions import HTTPException
+from .router import Router
 
-class App:
+class App(Router):
     def __init__(self):
-        self.routes: List[Tuple[str, Dict[str, Callable]]] = []
-        self.middleware: List[Callable] = []
+        super().__init__()
+        self.routers: List[Router] = []
         
     async def __call__(self, scope: dict, receive: Callable, send: Callable) -> None:
         """
@@ -29,31 +31,47 @@ class App:
         """
         Dispatch the request to the appropriate handler
         """
-        handler, params = self._get_handler(request.path, request.method)
-        if handler is None:
-            return Response({"error": "Not Found"}, status_code=404)
-            
         try:
-            # Add path parameters to request
+            handler, params = self._get_handler(request.path, request.method)
+            if handler is None:
+                raise NotFoundError(f"No route found for {request.path}")
+
             request.path_params = params or {}
             
-            # Run through middleware chain
             for middleware in self.middleware:
                 request = await middleware(request)
                 
             response = await handler(request)
             return response
+            
+        except HTTPException as e:
+            return Response(
+                {"error": e.detail},
+                status_code=e.status_code
+            )
         except Exception as e:
-            return Response({"error": str(e)}, status_code=500)
+            return Response(
+                {"error": "Internal Server Error", "detail": str(e)},
+                status_code=500
+            )
     
     def _get_handler(self, path: str, method: str) -> Tuple[Optional[Callable], Optional[Dict[str, str]]]:
         """
         Get the handler function and extracted parameters for a given path and method
         """
+        # First check app routes
         for route_pattern, methods in self.routes:
             params = match_route(route_pattern, path)
             if params is not None and method in methods:
                 return methods[method], params
+        
+        # Then check included routers
+        for router in self.routers:
+            for route_pattern, methods in router.routes:
+                params = match_route(route_pattern, path)
+                if params is not None and method in methods:
+                    return methods[method], params
+        
         return None, None
     
     def route(self, path: str, methods: List[str] = ["GET"]):
@@ -78,3 +96,9 @@ class App:
         
     def delete(self, path: str):
         return self.route(path, methods=["DELETE"])
+    
+    def include_router(self, router: Router, prefix: str = ""):
+        """Include a router with an optional prefix"""
+        if prefix:
+            router.prefix = prefix + router.prefix
+        self.routers.append(router)
