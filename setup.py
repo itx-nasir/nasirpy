fbb58152154cd61461cb0d9c3753787b42757ba5
NasirXpython <Nasir.Iqbal.Dev@gmail.com>
2022-07-13 13:00:00 +0000

    update-setup.py-to-fix-release-failing

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from setuptools import setup, find_packages
+import os
+
+# Read README file
+def read_readme():
+    readme_path = os.path.join(os.path.dirname(__file__), "README.md")
+    if os.path.exists(readme_path):
+        with open(readme_path, encoding="utf-8") as f:
+            return f.read()
+    return "A Python framework for learning how web frameworks work"
 
 setup(
     name="nasirpy",
@@ -6,7 +15,7 @@ setup(
     author="Nasir",
     author_email="Nasir.Iqbal.Dev@gmail.com",
     description="A Python framework for learning how web frameworks work",
-    long_description=open("README.md").read(),
+    long_description=read_readme(),
     long_description_content_type="text/markdown",
     url="https://github.com/itx-nasir/nasirpy",
     packages=find_packages(),
